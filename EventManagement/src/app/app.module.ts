301f9eecf90b3352c927344a5f76ac019de579c0
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2023-04-13 10:49:30 +0200

    ini

--- EventManagement/src/app/app.module.ts
+++ EventManagement/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { YonyComponent } from './test/yony/yony.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    YonyComponent
   ],
   imports: [
     BrowserModule,
