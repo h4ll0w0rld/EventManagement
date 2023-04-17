7c99a48dfde5e4b480accdd1e4dde939031d5a86
Jonathan Martens <j.martens@tks.eu>
2023-04-17 19:16:55 +0200

    stardern

--- EventManagement/src/app/app.module.ts
+++ EventManagement/src/app/app.module.ts
@@ -3,18 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { YonyComponent } from './test/yony/yony.component';
+import { ShiftPlanComponent } from './shift-plan/shift-plan.component';
+import { ShiftComponent } from './shift/shift.component';
+import { MenuComponent } from './menu/menu.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
-    YonyComponent
+    ShiftPlanComponent,
+    ShiftComponent,
+    MenuComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- EventManagement/src/app/shift-plan/shift-plan.component.spec.ts
+++ EventManagement/src/app/shift-plan/shift-plan.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TestComponent } from './test.component';
+import { ShiftPlanComponent } from './shift-plan.component';
 
-describe('TestComponent', () => {
-  let component: TestComponent;
-  let fixture: ComponentFixture<TestComponent>;
+describe('ShiftPlanComponent', () => {
+  let component: ShiftPlanComponent;
+  let fixture: ComponentFixture<ShiftPlanComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TestComponent ]
+      declarations: [ ShiftPlanComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(TestComponent);
+    fixture = TestBed.createComponent(ShiftPlanComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
