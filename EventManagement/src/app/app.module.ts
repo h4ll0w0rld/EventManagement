7cb37cd8c858c18ef1cf4bf5dddc94db9f34c814
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2023-05-04 11:38:05 +0200

    Update app.module.ts

--- EventManagement/src/app/app.module.ts
+++ EventManagement/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShiftPlanComponent } from './shift-plan/shift-plan.component';
 
 import { MenuComponent } from './menu/menu.component';
 
