e3780ca498eab7bf691439510b986ce84e412d1b
h4ll0w0rld <80950527+h4ll0w0rld@users.noreply.github.com>
2023-11-06 17:40:10 +0100

    naja

--- EventManagement/src/app/app-routing.module.ts
+++ EventManagement/src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShiftPlanComponent } from './Shiftplan Component/shift-plan/shift-plan.component';
 import { DashboardComponent } from './Dashboard Component/dashboard/dashboard.component';
+import { TesterComponent } from './tester/tester.component';
+import { ShiftplanLandingComponent } from './Shiftplan Component/shiftplan-landing/shiftplan-landing.component';
+import { EventHubComponent } from './event-hub/event-hub.component';
 
 
 const routes: Routes = [
   {path:'dashboard', component:DashboardComponent }, 
-  {path: '', component:ShiftPlanComponent },
-  // {
-  //   path: 'secondary',
-  //   loadChildren: () => import('./Shiftplan Component/shiftplan-landing/shiftplan-landing.component').then((m) => m.FeatureModule),
-  //   outlet: 'secondary', // Use the named outlet
-  // },
+  {path: 'shiftplan', component:ShiftPlanComponent },
+  {path: '', component:EventHubComponent},
+
 ];
 
 @NgModule({
