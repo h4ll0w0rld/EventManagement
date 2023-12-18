1c093047efffceafc996e55b09d5e9007325e0b6
YonyM <80950518+YonyM@users.noreply.github.com>
2023-12-18 19:39:34 +0100

    [NEW]
    auth-Components

---
+++ EventManagement/src/app/User/auth-landing/auth-landing.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-landing',
+  templateUrl: './auth-landing.component.html',
+  styleUrls: ['./auth-landing.component.scss'],
+  //encapsulation: ViewEncapsulation.None
+})
+export class AuthLandingComponent {
+
+}
